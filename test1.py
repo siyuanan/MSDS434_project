8db1b96c36194b3bcfc5e77dea10afe81d0648e9
siyuanan <97772736+siyuanan@users.noreply.github.com>
2022-01-14 21:56:16 -0600

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,6 +1,6 @@
 import numpy as np
 import test_func
 
-def test1(): 
-  result = test_func()
-  assert resutl == np.array([[1,2], [3,4], [5,6]])
+def test1():
+    result = test_func()
+    assert resutl == np.array([[1,2], [3,4], [5,6]])
