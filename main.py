cb0290b935b1f04c72b56e3c5f49016248022b8f
siyuanan <97772736+siyuanan@users.noreply.github.com>
2022-01-23 17:11:16 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return "Welcome to the new app!"
+
+if __name__ == "__main__":
+    app.run(host = '127.0.0.1', port = 8080, debug = True)
