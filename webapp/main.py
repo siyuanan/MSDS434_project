b95d4de4ef1ca809c38c3c5a1fd728f930ab339c
siyuanan <97772736+siyuanan@users.noreply.github.com>
2022-01-23 17:05:44 -0600

    Update main.py

--- webapp/main.py
+++ webapp/main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+efrom flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def welcome():
-    return "Hello to the new app!"
+    return "Welcome to the new app!"
 
 if __name__ == "__main__":
-    app.run(host = '0.0.0.0', port = 8080, debug = True)
+    app.run(host = '127.0.0.1', port = 8080, debug = True)
