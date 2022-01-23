9ba8cdb3cc5e715cdededc930b15cf745bf07794
siyuanan <97772736+siyuanan@users.noreply.github.com>
2022-01-23 17:08:59 -0600

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,5 +1,14 @@
 from test_func import test_func
+from webapp import main
 
 def test1():
     result = test_func()
     assert result == "Test"
+
+def main_test(): 
+    main.app.testing = True
+    client = main.app.test_client()
+
+    r = client.get('/')
+    assert r.status_code == 200
+    assert 'Welcome to the new app!' in r.data.decode('utf-8')
