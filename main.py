ec9768a1453e143d920fbec51aa7a7c7ce48db6d
siyuanan <97772736+siyuanan@users.noreply.github.com>
2022-02-06 09:35:05 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
 from flask import Flask
+from google.cloud import bigquery
+
+project_id  = 'msds434-2022-sa'
+dataset_id  = 'mobile_data'
+table_train = 'train'
+table_test  = 'test'
+table_synth = 'synthetic'
 
 app = Flask(__name__)
 
 def welcome():
-    return "Welcome to the new app! \nThis is for mobile price prediction"
+    return "Welcome to the new app!"
 
 def title_line():
-    return " This is for mobile price prediction"
+    return "This is for mobile price prediction"
+
+def pred_test():
+    query = f'''
+    SELECT * FROM {project_id}.{dataset_id}.synthetic order by rand() limit 10
+    '''
+    client = bigquery.Client(project = project_id)
+    query_job = client.query(query)
+    data = query_job.to_dataframe()
+    
+    return
+
 
 @app.route("/")
 def main_func(): 
-    return welcome() + '\n' + title_line()
+    return welcome() + '<br/><br/>' + title_line()
 
 if __name__ == "__main__":
     app.run(host = '127.0.0.1', port = 8080, debug = True)
