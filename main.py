880d5edb810102dbbe505b0dbb303f46164c812b
siyuanan <97772736+siyuanan@users.noreply.github.com>
2022-02-05 23:22:53 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ from flask import Flask
 
 app = Flask(__name__)
 
-# @app.route("/")
-# def welcome():
-#     return "Welcome to the new app!"
+def welcome():
+    return "Welcome to the new app!"
 
-@app.route("/")
 def title_line():
     return "This is for mobile price prediction"
+
+@app.route("/")
+def main_func(): 
+    print(welcome)
+    print('\n')
+    print(title_line)
 
 if __name__ == "__main__":
     app.run(host = '127.0.0.1', port = 8080, debug = True)
