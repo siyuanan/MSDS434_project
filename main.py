81ebd66c84e114ef37d6cf0f0824c228c20c44af
siyuanan <97772736+siyuanan@users.noreply.github.com>
2022-02-05 23:14:43 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def welcome():
-    return "Welcome to the new app!"
+# @app.route("/")
+# def welcome():
+#     return "Welcome to the new app!"
 
 @app.route("/")
 def title_line():
