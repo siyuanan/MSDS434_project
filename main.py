84bce8334dc960a3529f81a89309b788e66adc17
siyuanan <97772736+siyuanan@users.noreply.github.com>
2022-02-05 23:28:42 -0600

    Update main.py

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ def title_line():
 
 @app.route("/")
 def main_func(): 
-    print(welcome)
+    print(welcome())
     print('\n')
-    print(title_line)
+    print(title_line())
 
 if __name__ == "__main__":
     app.run(host = '127.0.0.1', port = 8080, debug = True)
