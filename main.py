8e8ed8967baa5aea2f9159822111e6e00e248412
siyuanan <97772736+siyuanan@users.noreply.github.com>
2022-02-27 12:59:17 -0600

    Update main.py

--- main.py
+++ main.py
@@ -115,7 +115,7 @@ def bill_plot():
     df2 = query_job.to_dataframe()
     
     data = pd.concat([df1, df2], ignore_index = True)
-    labels = list(data['usage_date'].dt.strftime('%Y-%m-%d'))
+    labels = list(pd.to_datetime(data['usage_date']).dt.strftime('%Y-%m-%d'))
     values = data['cost'].values.tolist()
     
 #     return render_template('view.html',tables = [data.to_html(classes='data')], titles = data.columns.values)
