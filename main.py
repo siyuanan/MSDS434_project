e9092b7c6cfc8dfca2467cf40bdc4693f228d7e2
Siyuan An <siyuanan2023@u.northwestern.edu>
2022-03-13 00:03:26 -0600

    deploy

--- main.py
+++ main.py
@@ -131,6 +131,7 @@ def pred_page():
     df_pred['score'] = 310 * df_pred['diff_ram'] + 185 * df_pred['diff_power'] \
                        + 137 * df_pred['diff_width'] + 128 * df_pred['diff_height']
     df_pred.sort_values(by = 'score', inplace = True, ignore_index = True)
+    table_pred = df_pred.head(1)[['predicted_label', 'predicted_label_probs']]
     pred = df_pred.loc[0, 'predicted_label']
     probs = df_pred.loc[0, 'predicted_label_probs']
 
@@ -139,6 +140,8 @@ def pred_page():
                            , form_data = form_data
                            , pred = pred
                            , probs = probs
+                           , table_pred = table_pred
+                           , title_pred = table_pred.columns.values
                            )
 
 
